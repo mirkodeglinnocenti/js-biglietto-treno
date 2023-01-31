effb074686666f18d40e26c26a12d685a4f8371a
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-01-31 15:41:23 +0100

    FEAT javascript exercise completed added

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+// chiedere all'utente il numero di chilometri che vuole percorrere
+const numberKm = parseFloat ( prompt ('Quanti chilometri desidera percorrere?'));
+console.log('Km che vuole percorrere:' , numberKm , 'Km');
+
+// chiedere all'utente la sua età
+const userAge = parseInt ( prompt ('Inserisca la sua età'));
+console.log('Età dell\'utente:' , userAge , 'anni');
+
+// calcolare il prezzo totale del viaggio
+let travelPrice;
+
+travelPrice = numberKm * 0.21;
+console.log('Prezzo pieno del biglietto:' , travelPrice.toFixed(2) , '€');
+
+// calcolare gli sconti se ci sono
+let discountTicket;
+
+if (userAge < 18) {
+
+    discountTicket = travelPrice * 0.8;
+    console.log('Prezzo scontato del biglietto:' , discountTicket.toFixed(2) , '€');
+
+}   else if (userAge > 65) {
+
+    discountTicket = travelPrice * 0.6;
+    console.log('Prezzo scontato del biglietto:' , discountTicket.toFixed(2) , '€');
+
+}
+
+// mostrare il prezzo finale con massimo due decimale
